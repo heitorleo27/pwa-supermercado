a6880ed3301adf6c31c6eefe7f55c5d9f19de37b
heitorleo27 <heitorleonardo2012@gmail.com>
2025-12-06 21:36:02 -0300

    fix: pwa config

--- vite.config.js
+++ vite.config.js
@@ -4,47 +4,48 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 
-  base: '/pwa-supermercado/', 
-  
+  base: '/pwa-supermercado/',
+
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        id: '/',
+        id: '/pwa-supermercado/',
         name: 'PWA Supermercado',
         short_name: 'Supermercado',
         description: 'Aplicativo PWA de supermercado feito com Vite + React',
-        
-      
-        theme_color: '#121212', 
+
+        start_url: '/pwa-supermercado/',
+        scope: '/pwa-supermercado/',
+
+        theme_color: '#121212',
         background_color: '#121212',
-        
         display: 'standalone',
-        scope: '/',
-        start_url: '/',
+
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: 'icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ],
+
         screenshots: [
           {
-            src: '/screenshots/home.png',
+            src: 'screenshots/home.png',
             sizes: '540x720',
             type: 'image/png',
             form_factor: 'narrow',
             label: 'Tela inicial'
           },
           {
-            src: '/screenshots/wide.png',
+            src: 'screenshots/wide.png',
             sizes: '720x540',
             type: 'image/png',
             form_factor: 'wide',
